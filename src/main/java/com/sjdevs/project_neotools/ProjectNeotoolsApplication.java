8402339ea6a35c49cc437e46c4538e06059a1b99
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2026-01-30 14:11:45 -0500

    feat: :tada: Inicializacion del proyecto

---
+++ src/main/java/com/sjdevs/project_neotools/ProjectNeotoolsApplication.java
@@ -0,0 +1,13 @@
+package com.sjdevs.project_neotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNeotoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNeotoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjdevs/project_neotools/ProjectNeotoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjdevs.project_neotools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNeotoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
